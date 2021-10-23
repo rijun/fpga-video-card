a2556f3d17e8ff34657f359a4dbaafef13263b79
rijun <richard.jung001@stud.fh-dortmund.de>
2021-10-23 16:53:54 +0200

    Update counter.vhd and synchronizer.vhd

--- fpga/rtl/counter.vhd
+++ fpga/rtl/counter.vhd
@@ -18,26 +18,44 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity counter is
---  Port ( );
+    port(i_clk       : in  std_logic;
+         i_resetn    : in  std_logic;
+         o_h_count : out std_logic_vector(9 downto 0);
+         o_v_count : out std_logic_vector(9 downto 0));
 end counter;
 
 architecture Behavioral of counter is
-
+    signal clk       : std_logic;
+    signal rst       : std_logic;
+    signal h_counter : unsigned(9 downto 0);
+    signal v_counter : unsigned(9 downto 0);
 begin
+    clk       <= i_clk;
+    rst       <= not i_resetn;
+    o_h_count <= std_logic_vector(h_counter);
+    o_v_count <= std_logic_vector(v_counter);
 
+    counter_proc : process(clk, rst)
+    begin
+        if (rst = '1') then
+            h_counter <= (others => '0');
+            v_counter <= (others => '0');
+        elsif (rising_edge(clk)) then
+            if (h_counter = 799) then
+                v_counter <= v_counter + 1;
+                h_counter <= (others => '0');
+            else
+                h_counter <= h_counter + 1;
+            end if;
 
+            if (v_counter = 520) then
+                v_counter <= (others => '0');
+            end if;
+        end if;
+    end process;
 end Behavioral;
--- fpga/rtl/synchronizer.vhd
+++ fpga/rtl/synchronizer.vhd
@@ -18,26 +18,69 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity synchronizer is
---  Port ( );
+    port(i_clk     : in  std_logic;
+         i_rst     : in  std_logic;
+         i_h_count : in  std_logic_vector(9 downto 0);
+         i_v_count : in  std_logic_vector(9 downto 0);
+         o_h_sync  : out std_logic;
+         o_v_sync  : out std_logic;
+         o_beam    : out std_logic;
+         o_pixel_x : out std_logic_vector(9 downto 0);
+         o_pixel_y : out std_logic_vector(8 downto 0));
 end synchronizer;
 
 architecture Behavioral of synchronizer is
-
+    signal clk     : std_logic;
+    signal rst     : std_logic;
+    signal h_count : unsigned;
+    signal v_count : unsigned;
 begin
+    clk     <= i_clk;
+    rst     <= i_rst;
+    h_count <= unsigned(i_h_count);
+    v_count <= unsigned(i_v_count);
 
+    sync_proc : process(clk, rst)
+    begin
+        if (rst = '1') then
+            o_h_sync <= '0';
+            o_v_sync <= '0';
+        elsif (rising_edge(clk)) then
+            if h_count <= 95 then
+                o_h_sync <= '0';
+            else
+                o_h_sync <= '1';
+            end if;
 
+            if v_count <= 2 then
+                o_v_sync <= '0';
+            else
+                o_v_sync <= '1';
+            end if;
+        end if;
+    end process;
+
+    pixel_proc : process(clk, rst)
+    begin
+        if (rst = '1') then
+            o_beam    <= '0';
+            o_pixel_x <= (others => '0');
+            o_pixel_y <= (others => '0');
+        elsif (rising_edge(clk)) then
+            if (h_count >= 144 and h_count <= 783 and v_count >= 31 and v_count <= 510) then
+                o_beam    <= '1';
+                o_pixel_x <= std_logic_vector(h_count - 144);
+                o_pixel_y <= std_logic_vector(v_count(8 downto 0) - 31);
+            else
+                o_beam    <= '0';
+                o_pixel_x <= (others => '0');
+                o_pixel_y <= (others => '0');
+            end if;
+        end if;
+    end process;
 end Behavioral;
